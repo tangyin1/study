8bf41c7f87f0fb2d693a7e53f86d764654de447c
tangyin <2283176194@qq.com>
2024-08-29 11:37:38 +0800

    qt仿360页面

---
+++ Qt-360-master/My360/main.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QTextCodec>
+#include <tr1/memory>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //QTextCodec::setCodecForTr(QTextCodec::codecForLocale());
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
+  //  QTextCodec::setCodecForCStrings(codec);
+    //QTextCodec::setCodecForTr(codec);
+
+    MainWindow* w(MainWindow::getInstance());
+    w->show();
+    MainWindow* w1(MainWindow::getInstance());
+    MainWindow* w2(MainWindow::getInstance());
+
+    w1->show();
+    w2->show();
+
+    return a.exec();
+}
